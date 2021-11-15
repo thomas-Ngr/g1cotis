24ca335d73c08df622821e7774f4bba5aaacdd46
tom ngr <dont@mail.me>
2021-11-15 14:07:59 +0000

    Add form to create user. Install annotations and apache-pack for route redirection.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     /**
      * @Route("/registration", name="registration")
      */
-    public function index(): Response
+    public function index(Request $request)
     {
+        $user = new User();
+
+        // This creates a "form" (?) according to the type "user" ??
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encode the new users password
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()));
+
+            // Set their role
+            $user->setRoles(['ROLE_USER']);
+
+            // Save
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'controller_name' => 'Registration',
+            'form' => $form->createView(),
         ]);
     }
 }
