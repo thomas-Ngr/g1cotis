bbb975b6c7709be9a7075b8cc15811054cb58aa6
tom ngr <dont@mail.me>
2021-11-16 10:48:56 +0000

    deal with registered users. Create user page redirection.

--- src/Controller/UserPageController.php
+++ src/Controller/UserPageController.php
@@ -4,17 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use  Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+use Symfony\Component\Security\Core\Security;
 
 class UserPageController extends AbstractController
 {
     /**
-     * @Route("/user/page", name="user_page")
+     * @Route("/user", name="user_page")
      */
-    public function index(): Response
+    public function userPage(Security $security): Response
     {
-        return $this->render('user_page/index.html.twig', [
-            'controller_name' => 'UserPageController',
-        ]);
+        if ($this->isGranted('ROLE_USER') == false) {
+            $error = new AuthenticationCredentialsNotFoundException(); // I would like to add a custom message...
+            return $this->render('security/login.html.twig', ['last_username' => '', 'error' => $error]);
+        }
+
+        $user = $security->getUser();
+        //dump($user); die();
+
+        if ($user) {
+            return $this->render('user_page/index.html.twig', [
+                'username' => $user->getUsername(),
+                'email' => $user->getEmail()
+                // TODO pass user accounts
+
+                // should I pass whole user entity ?
+            ]);
+        }
+        
+        // should I add a redirection to homepage in case of error ?
     }
 }
