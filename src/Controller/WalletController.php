7e840971136f2a7d0dcd8927161a83c15917a919
tom ngr <dont@mail.me>
2021-11-17 09:49:04 +0000

    Wallet creation:  - separate functions for GET or POST requests  - change user validation  - change logic to deal with multiple recipients (to verify further)

--- src/Controller/WalletController.php
+++ src/Controller/WalletController.php
@@ -7,9 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+use  Symfony\Component\Security\Core\Exception\AuthenticationCredentialsNotFoundException;
+use Symfony\Component\Security\Core\Security;
+
 use App\Entity\Wallet;
 use App\Entity\DispatchRecipient;
 use App\Form\CreateWalletType;
+
+
 
 /**
  * @Route("/user/wallet", name="wallet")
@@ -17,34 +22,37 @@ use App\Form\CreateWalletType;
 class WalletController extends AbstractController
 {
     /**
-     * @Route("/create", name="create_wallet")
+     * @Route("/create", name="create_wallet", methods={"POST"})
      */
-    public function create(Request $request): Response
+    public function create(Request $request, Security $security): Response
     {
-
+        
         /* REJECT USERS NOT LOGGED IN */
-
         if ($this->isGranted('ROLE_USER') == false) {
             $error = new AuthenticationCredentialsNotFoundException(); // I would like to add a custom message...
             return $this->render('security/login.html.twig', ['last_username' => '', 'error' => $error]);
         }
 
         /* BUILD FORM DATA */
+        $user = $security->getUser();
         $wallet = new Wallet();
 
-        // create 5 recipients forms
-        for ($i = 0 ; $i < 2 ; $i++) {
+        // get request content and create as much wallet recipients as sent by the user.
+        try {
+            $form_recipients = $request->request->get('create_wallet')['dispatchRecipients'];
+        } catch (Error $e) {
+            $error = new BadRequestHttpException('Wallet creation request is not correctly formatted');; // I would like to add a custom message...
+            return $this->redirectToRoute('/create');
+        }
+        
+        for ($i = 0 ; $i < count($form_recipients) ; $i++) {
             $recipient = new DispatchRecipient();
             $recipient->setWallet($wallet);
             $wallet->getDispatchRecipients()->add($recipient);
         }
+    
         $form = $this->createForm(CreateWalletType::class, $wallet);
-
         $form->handleRequest($request);
-
-        //dump($wallet); die();
-
-        $user = $this->getUser();
         
         /* MANAGE POST REQUESTS */
         if ($form->isSubmitted() && $form->isValid()) {
@@ -59,26 +67,50 @@ class WalletController extends AbstractController
             // wallet type (1 for dispatch. Should go in a constant.)
             $wallet->setType(1);
 
-
             // link wallet to user
             $wallet->setUser($user);
 
-            // ?? link recipients to wallet
-
             // Save
-            
             $em->persist($wallet);
             $em->flush();
 
-            return $this->redirectToRoute('/user'); // replace by wallet view
+            return $this->redirect('/user'); // replace by wallet view ?
         }
 
-        /* DISPLAY FORM IF WROND POST REQUEST */
+        // TODO deal with wrong POST requests
+    }
+
+    /**
+     * @Route("/create", name="create_wallet_form", methods={"GET","HEAD"})
+     */
+    public function displayForm(Request $request, Security $security): Response
+    {
+
+        /* REJECT USERS NOT LOGGED IN */    
+        if ($this->isGranted('ROLE_USER') == false) {
+            $error = new AuthenticationCredentialsNotFoundException(); // I would like to add a custom message...
+            return $this->render('security/login.html.twig', ['last_username' => '', 'error' => $error]);
+        }
+
+        /* BUILD FORM DATA */
+        $user = $security->getUser();
+        $wallet = new Wallet();
+
+        $recipient = new DispatchRecipient();
+        $recipient->setWallet($wallet);
+        $wallet->getDispatchRecipients()->add($recipient);
+
+        $form = $this->createForm(CreateWalletType::class, $wallet);
+
+        //dump($form); die(); // debug
+
+        /* DISPLAY FORM */
         return $this->render('wallet/create_form.html.twig', [
             'controller_name' => 'Create wallet',
             'form' => $form->createView(),
         ]);
     }
+
 
     /**
      * @Route("/{id}", name="view_account")
